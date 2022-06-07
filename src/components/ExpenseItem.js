2e836a137d4b71f786f64729a81706f60277ba67
Munjeong Choi <munjeong.dev@gmail.com>
2022-06-07 11:41:58 +0900

    섹션 3.39: 연습문제 정답

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,6 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
